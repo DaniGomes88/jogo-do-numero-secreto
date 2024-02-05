714f467755f05fe4ec202a1c5125262255589ddf
Daniel Gomes <104035378+DaniGomes88@users.noreply.github.com>
2024-02-05 17:43:15 -0300

    Dialeto manezês

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Quero ver su tú ésh ladino meixmo querido! Escolhe um número entre 1 e 10');
 }
 
 exibirMensagemInicial();
@@ -22,14 +22,14 @@ function verificarChute() {
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Parabéns!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        let mensagemTentativas = `Oooo, dazumbanho meishmo! Descobrisse o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
-            exibirTextoNaTela('p', 'O número secreto é menor');
+            exibirTextoNaTela('p', 'O número é menor um pouquinho oooo boca móli');
         } else {
-            exibirTextoNaTela('p', 'O número secreto é maior');
+            exibirTextoNaTela('p', 'O número é maior seu mazanza');
         }
         tentativas++;
         limparCampo();
